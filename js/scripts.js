1794ec2234056d0bf09bf7fd072632208c85eb60
sadegh azizi <sadeghazizi112@yahoo.com>
2019-02-28 14:26:26 -0800

    here goes nothing

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+var age;
+var gender;
+var genre;
+
+
+$(document).ready(function() {
+  $('#ryanreynolds').hide();
+  $('#dwainetherockjohnson').hide();
+  $('#jackblack').hide();
+  $("form#infoinput").submit(function(event) {
+
+  }
+}
