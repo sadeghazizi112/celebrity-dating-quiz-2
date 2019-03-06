13a43d3228561c48922da91ba59878f6c2a32f66
sadegh azizi <sadeghazizi112@yahoo.com>
2019-03-06 13:29:40 -0800

    here we go

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,36 @@ $(document).ready(function() {
   $('#ryanreynolds').hide();
   $('#dwainetherockjohnson').hide();
   $('#jackblack').hide();
+  $('#keanureeves').hide();
+  $('#sorry').hide();
   $("form#infoinput").submit(function(event) {
+    age = $("input#age").val();
+    gender = $("select#gender").val();
+    genre = $("select#genre").val();
 
-  }
+  $("#result").show();
+
+if (parseInt(age) >= 20 && (gender === 'female') && (genre === 'comedy')) {
+  $('#ryanreynolds').show();
 }
+
+if (parseInt(age) >= 20 && (gender === 'female') && (genre === 'action')) {
+  $('#keanureeves').show();
+}
+
+if (parseInt(age) >= 20 && (gender === 'male') && (genre === 'comedy')) {
+  $('#jackblack').show();
+}
+
+if (parseInt(age) >= 20 && (gender === 'male') && (genre === 'action')) {
+  $('#dwainetherockjohnson').show();
+}
+
+if (parseInt(age) < 20) {
+  $('#sorry').show();
+}
+
+event.preventDefault();
+
+});
+});
